0ed2403a2d4792258c21dc6bc1b277d95c0d03df
Vasil Vasilev <vasetousa@gmail.com>
2021-05-19 18:55:58 +0200

    new problems

--- Stacks and Queues/Robotics.py
+++ Stacks and Queues/Robotics.py
@@ -1,24 +1,38 @@
 from collections import deque
-from collections import defaultdict
-robots = input().split(';')
-data_robots = []
-for _ in robots:
+
+from datetime import datetime, timedelta
+
+data = input().split(';')
+time_now = datetime.strptime(input(), '%H:%M:%S')
+
+robots = []
+available_robots = deque([])
+for _ in data:
     name, time = _.split('-')
-    data_robots.append((name, time))
-start_time, minutes, seconds = input().split(':')
-minutes = int(minutes)
-seconds = int(seconds)
+    time = int(time)
+    robot = {}
+    robot['name'] = name
+    robot['processing_time'] = time
+    robot['available_at'] = time_now
+    robots.append(robot)
+    available_robots.append(robot)
+
 product = input()
-q = deque()
+q = deque([])
 
 while not product == 'End':
     q.append(product)
-
-
-
-
-
-
     product = input()
+
+time_now = time_now + timedelta(seconds=1)
+while product:
+    current_product = q.popleft()
+    if available_robots:
+        current_robot = available_robots.popleft()
+        current_robot['available_at'] = time_now + timedelta(seconds=current_robot['processing_time'])
+    else:
+        for r in robots:
+            if time_now >= r['available_at']:
+                available_robots.append(r)
 
 print()
