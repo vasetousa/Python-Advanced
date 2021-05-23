2f7052bab7fd1c64a30fd58f2cb3e70e1d24b4bf
Vasil Vasilev <vasetousa@gmail.com>
2021-05-23 12:07:18 +0200

    finished

---
+++ Truck Tour.py
@@ -0,0 +1,36 @@
+from collections import deque
+pumps = int(input())
+gas_stations = []
+index = deque(gas_stations)
+tank = 0
+counter = 0
+valid = True
+for t in range(pumps):
+    index.append(input())
+
+
+for el in range(pumps):
+    for _ in index:
+        petrol, distance = _.split()   # liters of petrol from this pump
+        petrol = int(petrol)     # from this pump to the next in km
+        distance = int(distance)     # from this pump to the next in km
+        tank += petrol
+        if tank >= distance:
+            tank -= distance
+            counter += 1
+        elif tank < distance:
+            tank_capacity = 0
+            index.rotate(-1)  # current station goes last, next station becomes 1st later
+            valid = False
+            counter = 0
+            tank = 0
+            break
+        if counter == len(index):
+            valid = True
+            break
+    if valid:
+        print(el)
+        break
+    else:
+        valid = True
+
